f2cf345ff592765ef1b319ee1a241ce3bfa97748
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-01-20 14:11:22 -0700

    grades

--- PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
+++ PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
@@ -6,6 +6,7 @@ submit.addEventListener("click", function(){
     let quiz = document.getElementById('quiz').value;
     let exam = document.getElementById('exams').value;
     let intex = document.getElementById('intex').value;
+    let grade = " "
 
     assignmentW = assignment * .5;
     projectW = project * .1;
@@ -15,5 +16,43 @@ submit.addEventListener("click", function(){
 
     total = assignmentW + projectW + quizW + examW + intexW; 
 
-    alert(total);
+    if (total >= 94) {
+        grade = 'A';
+    }
+    else if (total < 94 && total >= 90) {
+        grade = 'A-';
+    }
+    else if (total < 90 && total >= 87) {
+        grade = 'B+';
+    }
+    else if (total < 87 && total >= 84) {
+        grade = 'B';
+    }
+    else if (total < 84 && total >= 80) {
+        grade = 'B-';
+    }
+    else if (total < 80 && total >= 77) {
+        grade = 'C+';
+    }
+    else if (total < 77 && total >= 74) {
+        grade = 'C';
+    }
+    else if (total < 74 && total >= 70) {
+        grade = 'C-';
+    }
+    else if (total < 70 && total >= 67) {
+        grade = 'D+';
+    }
+    else if (total < 67 && total >= 64) {
+        grade = 'D';
+    }
+    else if (total < 64 && total >= 60) {
+        grade = 'D-';
+    }
+    else {
+        grade = 'E';
+    }
+
+
+    alert(total + ' ' + grade);
 });
