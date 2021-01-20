36c986f69e2d2642bb6592587e8c32e9af47d3b7
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-01-20 13:55:30 -0700

    initial calc

--- PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
+++ PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
@@ -1,5 +1,19 @@
 ﻿var submit = document.getElementById("submitbutton");
 
 submit.addEventListener("click", function(){
-   alert( document.getElementById('assignment').value);
+    let assignment = document.getElementById('assignment').value;
+    let project = document.getElementById('project').value;
+    let quiz = document.getElementById('quiz').value;
+    let exam = document.getElementById('exams').value;
+    let intex = document.getElementById('intex').value;
+
+    assignmentW = assignment * .5;
+    projectW = project * .1;
+    quizW = quiz * .1;
+    examW = exam * .2;
+    intexW = intex * .1; 
+
+    total = assignmentW + projectW + quizW + examW + intexW; 
+
+    alert(total);
 });
